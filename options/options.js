d392f2e6f7febf20efada41d1cbb917238f5e64f
Crowdbotics <team@crowdbotics.com>
2023-08-02 14:09:49 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "love-life-42590",
+  url: "https://love-life-42590.botics.co",
+  api: "https://love-life-42590.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
